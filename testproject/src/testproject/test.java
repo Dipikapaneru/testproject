d951331fa99d3f1b0f96322a60ed914b488f1e0c
dipikapaneru <dipikapaneru@gmail.com>
2020-03-06 13:20:43 -0500

    added Hi

--- testproject/src/testproject/test.java
+++ testproject/src/testproject/test.java
@@ -4,7 +4,11 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String greeting="Hi";
+		
 System.out.println("Hello World");
+System.out.println("Hi");
+
 	}
 
 }
