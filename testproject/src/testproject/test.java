4c70a9bbb0f9b62a629e9fab6cf5bcf71a70fd16
dipikapaneru <dipikapaneru@gmail.com>
2020-03-06 13:54:02 -0500

    remove name

--- testproject/src/testproject/test.java
+++ testproject/src/testproject/test.java
@@ -5,9 +5,10 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String greeting="Hi";
-		
+	
 System.out.println("Hello World");
 System.out.println("Hi");
+
 
 	}
 
