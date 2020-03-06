7ccf89b792b6a37e9f5dc99456b20d73dfa8dfc9
dipikapaneru <dipikapaneru@gmail.com>
2020-03-06 13:59:42 -0500

    addea name

--- testproject/src/testproject/test.java
+++ testproject/src/testproject/test.java
@@ -5,10 +5,10 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String greeting="Hi";
-	
+	String name="dipika";
 System.out.println("Hello World");
 System.out.println("Hi");
-
+System.out.println("dipika");
 
 	}
 
